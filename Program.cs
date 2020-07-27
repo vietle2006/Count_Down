04835fd2d193b01e34df9dbb248e8ad647e3cf30
vietle2006 <55930031+vietle2006@users.noreply.github.com>
2020-07-27 09:02:59 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Count_down
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v = 100; 
+
+            while (v > 0)
+            {
+                Console.WriteLine("the number is:{0}", v);
+
+                v--; 
+            }
+
+        }
+    }
+}
