da6a19585c801eae0ce65fc0c2444781b8229099
vietle2006 <55930031+vietle2006@users.noreply.github.com>
2020-07-27 10:18:36 -0400

    update files

---
+++ Count down/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Count_down
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+
+            Console.WriteLine("Enter a number that's higher than 0:");
+            x  = Convert.ToInt32(Console.ReadLine());
+
+
+            int v = x;
+
+            
+
+            
+
+            while (v > 0)
+            {
+                Console.WriteLine("the number is:{0}", v);
+
+                v--; 
+            }
+
+            
+            
+
+        }
+    }
+}
