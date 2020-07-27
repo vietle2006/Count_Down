557c0c76022a281efe34c2a6bbc0dc41ee79e80f
vietle2006 <55930031+vietle2006@users.noreply.github.com>
2020-07-27 14:02:42 -0400

    New Changes

--- Count down/Program.cs
+++ Count down/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Count_down
@@ -8,29 +9,65 @@ namespace Count_down
     {
         static void Main(string[] args)
         {
+
             int x;
 
 
             Console.WriteLine("Enter a number that's higher than 0:");
-            x  = Convert.ToInt32(Console.ReadLine());
-
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Loading.......");
+            StartDelay();
 
             int v = x;
 
-            
+          
 
-            
 
             while (v > 0)
             {
-                Console.WriteLine("the number is:{0}", v);
+             
+               
+                
 
-                v--; 
+                v--;
+
+                if (v % 2 == 0)
+                {
+                    Console.WriteLine("the number is:{0}", v);
+                    Console.Beep(75, 1000);
+                    
+                    
+
+                }
+                else
+                {
+                    Console.WriteLine("the number is:{0}", v);
+                    DelayTime();
+
+                }
+
+
+
+
+
+
             }
 
-            
-            
 
-        }
+            void DelayTime()
+            {
+                Thread.Sleep(1000);
+            }
+
+            void StartDelay()
+            {
+                Thread.Sleep(5000);
+            }
+
+
+            Console.Read();
+
+        }  
+            
     }
 }
